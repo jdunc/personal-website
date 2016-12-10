f8117ffa46b5166bf77cf66cedf1e8f4ea40ef25
Jordan Duncan <j.duncan@rochester.edu>
2016-12-10 17:11:36 -0600

    photo portfolio updated

--- assets/js/photo.js
+++ assets/js/photo.js
@@ -13,16 +13,42 @@ var msnry = new Masonry( '.grid', {
 $('img').each(function(i) {
 $(this).delay(i * 400).animate({
 opacity: 1,
-}, 3000, function() {
+}, 2000, function() {
 // Animation complete.
 });
 });
-$('h1').fadeIn(2500);
+$('h1').fadeIn(2000);
 $('h1').textillate({ in: { effect: 'flipInX', delayScale: 3,
 } });
 $('.grid-item').mouseenter(
  function(){
    console.log('hovered');
-   console.log($(event)[0]['target']['id']);
- }
-);
+   console.log($(event)[0]['target']);
+ });
+$('.grid-item').click(
+  function(){
+    console.log($(event)[0]['target']);
+    var selectedImg = $(event)[0]['target'];
+    $('.modal').append($('<div />', {
+        "class": 'modal-content',
+        text: "<",
+        }))
+    $(selectedImg).clone().appendTo('.modal-content');
+    // $('.modal > img').addClass('modal-content');
+    $('.modal-content').append('<a>></a>');
+    $('#myModal').show();
+  });
+// Get the modal
+var span = document.getElementsByClassName("close")[0];
+var myModal = document.getElementById('myModal');
+$('.close').click(function() {
+  $('#myModal').hide();
+  $('.modal-content').remove();
+});
+window.onclick = function(event) {
+  if (event.target == myModal) {
+    myModal.style.display = "none";
+    $('#myModal').empty();
+
+  }
+}
